e0348d39c4de66940d67f557f11abc28b9c91368
bluevariant <0x317374@gmail.com>
2020-12-30 17:04:40 +0700

    feat(Image): Resize.

--- src/pages/Images/index.vue
+++ src/pages/Images/index.vue
@@ -5,9 +5,31 @@
       <q-breadcrumbs-el label="Projects" to="/projects" />
       <q-breadcrumbs-el v-bind:label="$route.query.name" />
     </q-breadcrumbs>
+
     <q-card>
       <q-card-section>
-        <q-input v-model="filter" label="Search" clearable @focus="(e) => e.target.select()" />
+        <div class="q-gutter-xs justify-center items-center" style="height: 70px">
+          <q-input
+            dense
+            v-model="filter"
+            label="Search"
+            clearable
+            @focus="(e) => e.target.select()"
+            style="width: 300px"
+            class="inline-block q-mr-md"
+          />
+          <q-btn
+            flat
+            v-if="ticked.length > 0"
+            @click.stop="ticked = []"
+            color="negative"
+            icon-right="las la-times"
+          >
+            {{ ticked.length }} Ticked
+            <q-tooltip>Untick</q-tooltip>
+          </q-btn>
+          <q-btn flat v-if="ticked.length > 0" color="primary" @click.stop="resize">Resize</q-btn>
+        </div>
       </q-card-section>
       <q-card-section>
         <q-item v-if="items.length === 0">
@@ -20,7 +42,7 @@
             <q-tree
               :key="index"
               :nodes="[item]"
-              node-key="label"
+              node-key="id"
               tick-strategy="leaf"
               :ticked.sync="ticked"
               :expanded.sync="expanded"
@@ -51,6 +73,7 @@
 <script>
 import readDir from "src/utils/readDir";
 import _ from "lodash";
+import jimp from "jimp";
 
 const path = require("path");
 const fs = require("fs-extra");
@@ -72,56 +95,91 @@ export default {
     return getDefaultData(this);
   },
   mounted() {
-    this.$q.loading.show();
-    readDir(this.$route.query.path).then((files) => {
-      let items = _.filter(files, (file) => {
-        let whiteExtensions = ["png", "jpg", "gif", "jpeg"];
-        for (let ext of whiteExtensions) {
-          if (file.endsWith("." + ext)) return true;
-        }
-        return false;
-      });
-      let dataset = [];
-      let ids = {};
-      _.forEach(items, (fullPath) => {
-        let filePath = path
-          .relative(this.$route.query.path || "", fullPath.toString())
-          .replace(/\\/g, "/");
-        let splitedPath = filePath.split("/");
-        while (splitedPath.length > 0) {
-          let fileId = splitedPath.join("/");
-          if (!ids[fileId]) {
-            let flatItem = {
-              id: fileId,
-              label: splitedPath[splitedPath.length - 1],
-              path: fullPath,
-              isFile: fs.lstatSync(path.join(this.$route.query.path || "", fileId)).isFile(),
-            };
-            splitedPath.pop();
-            if (splitedPath.length > 0) {
-              flatItem.parentId = splitedPath.join("/");
-            }
-            if (flatItem.isFile) {
-              flatItem.header = "file";
-            }
-            dataset.push(flatItem);
-            ids[fileId] = true;
-          } else {
-            splitedPath.pop();
-          }
-        }
-      });
-      this.items = Object.freeze(listToTree(dataset));
-      console.log(this.items);
-      this.$q.loading.hide();
-      this.loaded = true;
-    });
+    this.loadTree();
   },
   methods: {
     openImage(path) {
       if (path.endsWith(".gif")) {
         window.open(path, "_blank");
       } else shell.openPath(path);
+    },
+    resize() {
+      this.$q
+        .dialog({
+          title: "Prompt",
+          message: "Size",
+          prompt: {
+            model: 640,
+            type: "number",
+            attrs: {
+              onfocus: (e) => e.target.select(),
+            },
+            suffix: "px",
+          },
+          cancel: true,
+          persistent: true,
+        })
+        .onOk(async (size) => {
+          this.$q.loading.show();
+          for (let fileId of this.ticked) {
+            let filePath = path.join(this.$route.query.path || "", fileId);
+            let image = await jimp.read(filePath);
+            if (image.getWidth() > image.getHeight()) {
+              await image.resize(size, jimp.AUTO);
+            } else {
+              await image.resize(jimp.AUTO, size);
+            }
+            await image.writeAsync(filePath);
+            console.log("resized: " + fileId);
+          }
+          this.$q.loading.hide();
+        });
+    },
+    loadTree() {
+      this.$q.loading.show();
+      readDir(this.$route.query.path).then((files) => {
+        let items = _.filter(files, (file) => {
+          let whiteExtensions = ["png", "jpg", "gif", "jpeg"];
+          for (let ext of whiteExtensions) {
+            if (file.endsWith("." + ext)) return true;
+          }
+          return false;
+        });
+        let dataset = [];
+        let ids = {};
+        _.forEach(items, (fullPath) => {
+          let filePath = path
+            .relative(this.$route.query.path || "", fullPath.toString())
+            .replace(/\\/g, "/");
+          let splitedPath = filePath.split("/");
+          while (splitedPath.length > 0) {
+            let fileId = splitedPath.join("/");
+            if (!ids[fileId]) {
+              let flatItem = {
+                id: fileId,
+                label: splitedPath[splitedPath.length - 1],
+                path: fullPath,
+                isFile: fs.lstatSync(path.join(this.$route.query.path || "", fileId)).isFile(),
+              };
+              splitedPath.pop();
+              if (splitedPath.length > 0) {
+                flatItem.parentId = splitedPath.join("/");
+              }
+              if (flatItem.isFile) {
+                flatItem.header = "file";
+              }
+              dataset.push(flatItem);
+              ids[fileId] = true;
+            } else {
+              splitedPath.pop();
+            }
+          }
+        });
+        this.items = Object.freeze(listToTree(dataset));
+        console.log(this.items);
+        this.$q.loading.hide();
+        this.loaded = true;
+      });
     },
   },
 };
