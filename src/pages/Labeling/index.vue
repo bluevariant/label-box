705da22a82fcde50ebdf7e270e9e5899a394a0ae
bluevariant <0x317374@gmail.com>
2021-01-03 16:49:24 +0700

    feat(Projects): Show image.

--- src/pages/Labeling/index.vue
+++ src/pages/Labeling/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page padding class="q-gutter-md">
+  <q-page padding class="q-gutter-md user-select-none">
     <q-breadcrumbs>
       <q-breadcrumbs-el icon="las la-home" to="/" />
       <q-breadcrumbs-el label="Projects" to="/projects" />
@@ -7,17 +7,34 @@
     </q-breadcrumbs>
     <q-card>
       <q-card-section>
-        <div class="row">
-          <div class="col-3 left-side">
+        <div class="row q-col-gutter-md">
+          <div class="col-3 left-side q-mt-md q-pt-none">
             <div class="row q-col-gutter-sm">
-              <div class="col-6" v-for="(src, index) in images" :key="index">
-                <div :class="selected === index ? 'active' : 'no-active'" @click="selected = index">
-                  <img :src="src" alt="thumbnail" class="thumbnail" />
+              <div
+                class="col-6"
+                v-for="(src, index) in images"
+                :key="index"
+                @click="selected = index"
+              >
+                <div
+                  class="items-center justify-center flex bg-black"
+                  :class="selected === index ? 'active' : 'no-active'"
+                  style="height: 100%; width: 100%"
+                >
+                  <span class="inline-block">
+                    <img :src="src" alt="thumbnail" class="thumbnail block" />
+                  </span>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col-9">Hello</div>
+          <div class="col-9">
+            <div class="right-side items-center justify-center flex">
+              <span class="text-white inline-block image-container">
+                <img v-if="selected >= 0" :src="images[selected]" alt="thumbnail" />
+              </span>
+            </div>
+          </div>
         </div>
       </q-card-section>
     </q-card>
@@ -96,15 +113,44 @@ export default {
 
 .active {
   border: 4px solid var(--q-color-primary);
+}
+
+.active img {
   opacity: 1;
 }
 
 .no-active {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+}
+
+.no-active img {
   opacity: 0.5;
 }
 
 .left-side {
-  height: calc(100vh - 160px);
+  height: calc(100vh - 155px);
   overflow-y: auto;
+}
+
+.right-side {
+  width: 100%;
+  height: 100%;
+  background: #000000;
+}
+
+.image-container {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.image-container img {
+  display: block;
+  max-height: calc(100vh - 155px);
+  max-width: 100%;
+  pointer-events: none;
+}
+
+.user-select-none {
+  user-select: none;
 }
 </style>
